13ea0e68fa54c6b5cd3af1d597a6ddd2b4d9080d
Web Developer <otabekahmatjonov5@gmail.com>
2023-07-27 18:46:15 +0500

    ketvotti

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-
-function App() {
-  return 
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1>Asosiy block</h1>
+      <Footer />
+    </>
+  );
+};
 
 export default App;

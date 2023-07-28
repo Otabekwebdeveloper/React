5797ebd95b5bee9285c06869e9f995dd2e2e63b2
Web Developer <otabekahmatjonov5@gmail.com>
2023-07-28 21:36:08 +0500

    pashti tayyor

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY } from "../config";
+import Loader from "./Loader";
 
 export default function Shop() {
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL, { headers: { Authorization: API_KEY } })
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data.featured);
+        setLoading(false);
+      });
+  });
+
   return (
-    <div>Shop</div>
-  )
+    <div className="container content">
+      {loading ? <Loader /> : <h1>sdad</h1>}
+    </div>
+  );
 }
